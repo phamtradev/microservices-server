6b0998262dac3da6c0d23637c67dcc5dbd0b616b
Phạm Trà <phamvantra.dev@gmail.com>
2026-03-12 22:32:03 +0700

    feat: add Permission, Role, RefreshToken, and VerificationToken entities for access control

---
+++ identity-service/src/main/java/com/phamtra/identity_service/model/Permission.java
@@ -0,0 +1,30 @@
+package com.phamtra.identity_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "permissions")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private String description;
+}
