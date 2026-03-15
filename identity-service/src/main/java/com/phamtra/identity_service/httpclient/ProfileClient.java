4f47e36c5de64944e3f953531a36738f0f30697f
Phạm Trà <phamvantra.dev@gmail.com>
2026-03-15 14:08:22 +0700

    feat: update createUserProfile endpoint path to /profile/internal/users for better API structure

--- identity-service/src/main/java/com/phamtra/identity_service/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/phamtra/identity_service/httpclient/ProfileClient.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "profile-service", url = "${app.services.profile}")
 public interface ProfileClient {
 
-    @PostMapping(value = "/profile", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/profile/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     CreateProfileResponse createUserProfile(@RequestBody CreateProfileRequest request);
 }
